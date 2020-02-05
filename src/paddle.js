6dcb6f9aab20a8aca3216e859057b3da277c1be7
ThomasMcKnight <thomasmcknight711@gmail.com>
2020-02-04 19:48:28 -0500

    Added movement to paddle with player movement

--- src/paddle.js
+++ src/paddle.js
@@ -1,7 +1,11 @@
 export default class Paddle {
     constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth;
         this.width = 150;
         this.height = 30;
+
+        this.maxSpeed = 7;
+        this.speed = 0;
 
         this.position = {
             x: gameWidth / 2 - this.width / 2,
@@ -9,8 +13,26 @@ export default class Paddle {
         };
     }
 
+    moveLeft() {
+        this.speed = -this.maxSpeed;
+    }
+
+    moveRight() {
+        this.speed = this.maxSpeed;
+    }
+
     draw(ctx) {
+        ctx.fillStyle = "#0ff";
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update(deltaTime) {
+        if(!deltaTime) return;
+
+        this.position.x += this.speed;
+
+        if(this.position.x < 0) this.position.x = 0;
+        if(this.position.x + this.width > this.gameWidth) this.position.x = 650;
 
     }
 }

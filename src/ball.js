c170807550d1072aa9479ed1182c7900e1f7f57a
ThomasMcKnight <thomasmcknight711@gmail.com>
2020-02-04 21:28:38 -0500

    Created ball/movement

---
+++ src/ball.js
@@ -0,0 +1,29 @@
+export default class Ball {
+    constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+        this.image = document.getElementById("img_ball");
+        this.speed = {x: 2, y: 2};
+        this.position = {x: 10, y: 10};
+        this.size = 10;
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.image, this.position.x, this.position.y, this.size, this.size);
+    }
+
+    update(deltaTime) {
+        this.position.x += this.speed.x;
+        this.position.y += this.speed.y;
+
+        if(this.position.x > this.gameWidth || this.position.x < 0) {
+            this.speed.x = -this.speed.x;
+        }
+
+        if(this.position.y > this.gameHeight || this.position.y < 0) {
+            this.speed.y = -this.speed.y;
+        }
+
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Paddle from '/src/paddle.js';
 import InputHandler from '/src/input.js';
+import Ball from '/src/ball.js';
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -8,23 +9,24 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-
 new InputHandler(paddle);
-
-paddle.draw(ctx);
+let ball = new Ball(GAME_WIDTH, GAME_HEIGHT);
 
 let lastTime = 0;
+
 
 function gameLoop(timestamp) {
 
     let deltaTime = timestamp - lastTime;
     lastTime = timestamp;
 
-    ctx.clearRect(0, 0, 800, 600);
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     paddle.update(deltaTime);
     paddle.draw(ctx);
+    ball.update(deltaTime);
+    ball.draw(ctx);
 
     requestAnimationFrame(gameLoop);
 }
 
-gameLoop();
+requestAnimationFrame(gameLoop);

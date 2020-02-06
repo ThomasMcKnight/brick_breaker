9c238f2a0c4fdaeef6fce0fe7ebb2216ecc5933e
ThomasMcKnight <thomasmcknight711@gmail.com>
2020-02-05 21:42:28 -0500

    Added bricks

--- src/game.js
+++ src/game.js
@@ -1,6 +1,9 @@
 import Paddle from '/src/paddle.js';
 import InputHandler from '/src/input.js';
 import Ball from '/src/ball.js';
+import Brick from '/src/brick.js';
+
+import { buildLevel, level1 } from '/src/levels.js';
 
 export default class Game {
     constructor(gameWidth, gameHeight) {
@@ -11,8 +14,10 @@ export default class Game {
     start() {
         this.ball = new Ball(this);
         this.paddle = new Paddle(this);
-        this.gameObjects = [this.ball, this.paddle];
-        new InputHandler(this.paddle);
+        let bricks = buildLevel(this, level1);
+        
+        this.gameObjects = [this.ball, this.paddle, ...bricks];
+        new InputHandler(this.paddle);      
     }
 
     update(deltaTime) {
